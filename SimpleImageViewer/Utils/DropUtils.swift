9240666115a2d5f0e5b4a8abd65e546f2d68a79e
Gualtiero <gualtiero.frigerio@gmail.com>
2021-02-05 17:32:50 +0100

    added DropUtils

---
+++ SimpleImageViewer/Utils/DropUtils.swift
@@ -0,0 +1,27 @@
+//
+//  DropUtils.swift
+//  SimpleImageViewer
+//
+//  Created by Gualtiero Frigerio on 05/02/21.
+//
+
+import Foundation
+import SwiftUI
+
+class DropUtils {
+    class func urlFromDropInfo(_ info:DropInfo, completion: @escaping (URL?) -> Void)  {
+        guard let itemProvider = info.itemProviders(for: [(kUTTypeFileURL as String)]).first else {
+            completion(nil)
+            return
+        }
+
+        itemProvider.loadItem(forTypeIdentifier: (kUTTypeFileURL as String), options: nil) {item, error in
+            guard let data = item as? Data,
+                  let url = URL(dataRepresentation: data, relativeTo: nil) else {
+                completion(nil)
+                return
+            }
+            completion(url)
+        }
+    }
+}
