c006a13759cbb601303e581fc89892f8f713cf91
Gualtiero <gualtiero.frigerio@gmail.com>
2021-02-09 16:28:04 +0100

    added MenuCommands

---
+++ SimpleImageViewer/Views/MenuCommands.swift
@@ -0,0 +1,21 @@
+//
+//  MenuCommands.swift
+//  SimpleImageViewer
+//
+//  Created by Gualtiero Frigerio on 08/02/21.
+//
+
+import SwiftUI
+
+struct MenuCommands {
+    var commandsHandler:MenuCommandsHandler
+    
+    var commands: some Commands {
+        CommandGroup(after: CommandGroupPlacement.newItem) {
+            Button("Open...") {
+                commandsHandler.openCommand()
+            }
+        }
+    }
+}
+
