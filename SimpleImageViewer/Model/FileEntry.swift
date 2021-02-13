a8b4d4296d8d75d2c22444f011003c841e99a8dd
Gualtiero <gualtiero.frigerio@gmail.com>
2021-02-13 18:24:12 +0100

    added createFromFileString and Equatable conformance

--- SimpleImageViewer/Model/FileEntry.swift
+++ SimpleImageViewer/Model/FileEntry.swift
@@ -10,12 +10,35 @@ import Foundation
 /// Describes a filesystem entry
 struct FileEntry {
     var isDir:Bool // true if it is a directory
-    var fileURL:URL // full path url
+    var isFavorite:Bool = false // true if is part of the user favorites
+    var fileURL:URL // full path url string
     var name:String // file/directory name
+}
+
+extension FileEntry {
+    static func createFromFileString(_ path:String) -> Self? {
+        let absolutePath = path.replacingOccurrences(of: "file://", with: "")
+        let fileManager = FileManager.default
+        var isDir:ObjCBool = false
+        let exists = fileManager.fileExists(atPath: absolutePath, isDirectory: &isDir)
+        if exists == false {
+            return nil
+        }
+        let url = URL(fileURLWithPath: absolutePath)
+        let name = url.lastPathComponent
+        
+        return FileEntry(isDir: isDir.boolValue, fileURL: url, name: name)
+    }
 }
 
 extension FileEntry:Identifiable {
     var id: String {
         name
+    }
+}
+
+extension FileEntry:Equatable {
+    static func ==(lhs:FileEntry, rhs:FileEntry) -> Bool {
+        lhs.fileURL.absoluteString == rhs.fileURL.absoluteString
     }
 }
