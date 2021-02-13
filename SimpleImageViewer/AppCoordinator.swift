3ac64a0a1271870f52c458bd7ef74be876e2d770
Gualtiero <gualtiero.frigerio@gmail.com>
2021-02-13 18:23:19 +0100

    refactoring

--- SimpleImageViewer/AppCoordinator.swift
+++ SimpleImageViewer/AppCoordinator.swift
@@ -8,13 +8,27 @@
 import Foundation
 
 class AppCoordinator {
-    private (set) var fileViewModel:FilesViewModel = FilesViewModel()
+    private (set) var filesViewModel:FilesViewModel
+    
+    init() {
+        favoritesManager = FavoritesManager()
+        filesViewModel = FilesViewModel(favoritesManager: favoritesManager)
+    }
     
     func setDirectory(_ url:URL) {
-        fileViewModel.setDirectory(url)
+        filesViewModel.setDirectory(url)
     }
     
     func showFavorites() {
-        
+        let allFavorites = favoritesManager.favorites.compactMap {
+            FileEntry.createFromFileString($0)
+        }
+        filesViewModel.updateEntries(allFavorites)
     }
+    
+    func toogleFavorite(forFileEntry entry:FileEntry) {
+        favoritesManager.toggle(fileEntry: entry)
+    }
+    
+    private var favoritesManager:FavoritesManager
 }
