e7b17d9d98f35b7d9c7f4946372c8672cfc3f32c
Gualtiero <gualtiero.frigerio@gmail.com>
2021-02-19 10:36:57 +0100

    added AppScene struct

--- SimpleImageViewer/AppDelegate.swift
+++ SimpleImageViewer/AppDelegate.swift
@@ -12,39 +12,47 @@ import SwiftUI
  If you can target macOS 11 you can use Scene
  */
 
+
 @main
 struct SimpleImageViewer: App {
     var coordinator:AppCoordinator
     var menuCommandsHandler:MenuCommandsHandler
-    
+        
     init() {
         coordinator = AppCoordinator()
         menuCommandsHandler = MenuCommandsHandler(coordinator: coordinator)
     }
     
     var body: some Scene {
+        AppScene(coordinator: coordinator, menuCommandsHandler: menuCommandsHandler)
+    }
+}
+
+struct AppScene:Scene {
+    var coordinator:AppCoordinator
+    var menuCommandsHandler:MenuCommandsHandler
+    
+    var body: some Scene {
         WindowGroup {
             ContentView(coordinator: coordinator)
         }.commands {
-            MenuCommands(commandsHandler: menuCommandsHandler).commands
+            MenuCommands(commandsHandler: menuCommandsHandler)
         }
     }
 }
 
-/*
 
- this was created by Xcode when I started the macOS project
- 
+ // this was created by Xcode when I started the macOS project
+/*
 @main
 class AppDelegate: NSObject, NSApplicationDelegate {
 
     var window: NSWindow!
 
-
     func applicationDidFinishLaunching(_ aNotification: Notification) {
         // Create the SwiftUI view that provides the window contents.
         let contentView = ContentView()
-
+        
         // Create the window and set the content view.
         window = NSWindow(
             contentRect: NSRect(x: 0, y: 0, width: 480, height: 300),
@@ -63,4 +71,5 @@ class AppDelegate: NSObject, NSApplicationDelegate {
 
 
 }
+
 */
