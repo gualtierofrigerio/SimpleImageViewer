f7ffaadcdb0e2dca5f84f2127b53b1147af1c432
Gualtiero <gualtiero.frigerio@gmail.com>
2021-02-27 16:04:19 +0100

    rewritten with LazyVStack

--- SimpleImageViewer/Views/FilesView.swift
+++ SimpleImageViewer/Views/FilesView.swift
@@ -9,6 +9,7 @@ import SwiftUI
 
 struct FilesView: View {
     @ObservedObject var viewModel:FilesViewModel
+    @State var openImage = false
     
     init(coordinator:AppCoordinator) {
         self.coordinator = coordinator
@@ -16,6 +17,51 @@ struct FilesView: View {
     }
     
     var body: some View {
+        //listView() // uncomment to use old implementation with List
+        NavigationLink(destination: SingleImageView(imageURL:selectedImageURL), isActive: $openImage){}.hidden()
+        ScrollView {
+            LazyVStack(alignment: .leading) {
+                ForEach(viewModel.entries) { entry in
+                    if entry.isDir {
+                        Button(action:{
+                                selectDirectory(entry:entry)}) {
+                            SingleEntryView(buttonAction:emptyAction, entry: entry)
+                        }.buttonStyle(PlainButtonStyle())
+                    }
+                    else {
+                        Button(action: {
+                            self.selectedImageURL = entry.fileURL
+                            self.openImage = true
+                        }) {
+                            SingleEntryView(buttonAction:{
+                                toggleFavorite(entry)
+                            }, entry: entry)
+                            .frame(width:nil, height:250)
+                        }.buttonStyle(PlainButtonStyle())
+                    }
+                }
+            }
+        }
+        .onDrop(of: ["public.file-url"], delegate: self)
+    }
+    
+    // MARK: - Private
+    private var coordinator:AppCoordinator
+    private var emptyAction:() -> Void = {} // used on dir entries as you don't need an action
+    @State private var selectedImageURL:URL = URL(string:"url")!
+    
+    private func selectDirectory(entry:FileEntry) {
+        coordinator.setDirectory(entry.fileURL)
+    }
+    
+    private func toggleFavorite(_ entry:FileEntry) {
+        coordinator.toogleFavorite(forFileEntry: entry)
+    }
+}
+
+// old implementation with List and NavigationLink
+extension FilesView {
+    private func listView() -> some View {
         List(viewModel.entries) { entry in
             if entry.isDir {
                 Button(action:{
@@ -32,19 +78,6 @@ struct FilesView: View {
                 }
             }
         }
-        .onDrop(of: ["public.file-url"], delegate: self)
-    }
-    
-    // MARK: - Private
-    private var coordinator:AppCoordinator
-    private var emptyAction:() -> Void = {} // used on dir entries as you don't need an action
-    
-    private func selectDirectory(entry:FileEntry) {
-        coordinator.setDirectory(entry.fileURL)
-    }
-    
-    private func toggleFavorite(_ entry:FileEntry) {
-        coordinator.toogleFavorite(forFileEntry: entry)
     }
 }
 
