987223458cce6120a43b727440bd78d376b18d1b
Gualtiero <gualtiero.frigerio@gmail.com>
2021-09-13 12:03:38 +0200

    added SingleEntryViewModel

---
+++ SimpleImageViewer/Model/SingleEntryViewModel.swift
@@ -0,0 +1,31 @@
+//
+//  SingleEntryViewModel.swift
+//  SingleEntryViewModel
+//
+//  Created by Gualtiero Frigerio on 12/09/21.
+//
+
+import Foundation
+import Combine
+
+class SingleEntryViewModel: ObservableObject {
+    var buttonAction: () -> Void
+    var entry: FileEntry
+    
+    init(entry: FileEntry, coordinator: AppCoordinator) {
+        self.entry = entry
+        self.coordinator = coordinator
+        self.buttonAction = {}
+        if !entry.isDir {
+            buttonAction = toggleFavorite
+        }
+    }
+    
+    private var coordinator: AppCoordinator
+    
+    private func toggleFavorite() {
+        coordinator.toogleFavorite(forFileEntry: entry)
+        entry.isFavorite = coordinator.favoritesManager.isFavorite(entry)
+        objectWillChange.send()
+    }
+}
