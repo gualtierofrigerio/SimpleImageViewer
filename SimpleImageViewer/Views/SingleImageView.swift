b6cc1be085f0d554594b197f10f850148fa8d60e
Gualtiero <gualtiero.frigerio@gmail.com>
2021-10-03 19:38:27 +0200

    added SingleImageViewGestureState

--- SimpleImageViewer/Views/SingleImageView.swift
+++ SimpleImageViewer/Views/SingleImageView.swift
@@ -27,6 +27,26 @@ struct SingleImageView: View {
     }
 }
 
+struct SingleImageViewGestureState: View {
+    var imageURL: URL
+    var containerSize: CGSize
+        
+    @GestureState var scale: CGFloat = 1.0
+    
+    var body: some View {
+        ImageView(withURL: imageURL)
+            .frame(width: containerSize.width * scale,
+                   height: containerSize.height * scale)
+            .gesture(MagnificationGesture()
+                    .updating($scale) { currentState, gestureState, transaction in
+                        gestureState = currentState
+                    }
+                    )
+        Text(imageURL.lastPathComponent)
+    }
+}
+
+
 struct SingleImageView_Previews: PreviewProvider {
     static var previews: some View {
         SingleImageView(imageURL: URL(string: "")!, containerSize: CGSize(width: 0, height: 0))
