aee6ad8ba3907ca1b95eff11d674b5a80dcfe9e5
Gualtiero <gualtiero.frigerio@gmail.com>
2021-10-31 17:30:34 +0100

    removed Slider

--- SimpleImageViewer/Views/SingleImageView.swift
+++ SimpleImageViewer/Views/SingleImageView.swift
@@ -14,7 +14,6 @@ struct SingleImageView: View {
     @ObservedObject var viewModel = SingleImageViewModel()
     
     var body: some View {
-        Slider(value: $viewModel.sliderValue, in: 0.5...3)
         ImageView(withURL: imageURL)
             .frame(width: containerSize.width * viewModel.scale,
                    height: containerSize.height * viewModel.scale)
