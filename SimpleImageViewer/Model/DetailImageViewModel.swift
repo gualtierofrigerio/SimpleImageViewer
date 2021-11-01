b30440131f11d34f77cd0e1d773b2243370a3176
Gualtiero <gualtiero.frigerio@gmail.com>
2021-11-01 13:21:40 +0100

    added SingleImageViewModel stepperValue and currentZoomDisplay

--- SimpleImageViewer/Model/DetailImageViewModel.swift
+++ SimpleImageViewer/Model/DetailImageViewModel.swift
@@ -14,10 +14,32 @@ import Combine
 /// calling showImage sets a @Published var that forces the update of the view
 class DetailImageViewModel: ObservableObject {
     @Published var imageURL: URL?
+    @Published var currentZoomDisplay: String = "100 %"
+    
+    @Published var stepperValue: Int = 1 {
+        didSet {
+            singleImageViewModel.scale = 1.0 + CGFloat(stepperValue) / 10
+        }
+    }
+    
+    // TODO: - find a better solution to handle this as an optional or initialize it without a URL
+    var singleImageViewModel: SingleImageViewModel = SingleImageViewModel(imageURL: URL(string: "https://google.com")!)
     
     /// Loads an image forcing the view to show it
     /// - Parameter url: the image url
     func showImage(atURL url: URL) {
         imageURL = url
+        singleImageViewModel = SingleImageViewModel(imageURL: url)
+        cancellable = singleImageViewModel.$scale.sink { value in
+            self.updateZoom(withScale: value)
+        }
+    }
+
+    // MARK: - Private
+    
+    private var cancellable: AnyCancellable?
+    
+    private func updateZoom(withScale scale: CGFloat) {
+        currentZoomDisplay = String(format: "%.2f", scale * 100) + " %"
     }
 }
