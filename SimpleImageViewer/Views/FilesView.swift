a6d5469a763d7bf190f8fa72183bee7f106b18f0
Gualtiero <gualtiero.frigerio@gmail.com>
2021-12-17 10:33:13 +0100

    refactoring to simply body

--- SimpleImageViewer/Views/FilesView.swift
+++ SimpleImageViewer/Views/FilesView.swift
@@ -19,7 +19,6 @@ struct FilesView: View {
     }
     
     var body: some View {
-        //listView() // uncomment to use old implementation with List
         NavigationLink(destination: DetailImageView(viewModel: coordinator.detailImageViewModel),
                        isActive: $imageActive){}.hidden()
         NavigationLink(destination: DetailVideoView(viewModel: coordinator.detailVideoViewModel),
@@ -30,29 +29,7 @@ struct FilesView: View {
         ScrollView {
             LazyVStack(alignment: .leading) {
                 ForEach(viewModel.entries) { entry in
-                    switch entry.type {
-                    case .directory:
-                        Button(action:{
-                            selectDirectory(entry:entry)}) {
-                                SingleEntryView(viewModel: viewModel(forEntry: entry))
-                            }.buttonStyle(PlainButtonStyle())
-                    case .image:
-                        Button(action: {
-                            coordinator.showImage(atURL: entry.fileURL)
-                            imageActive = true
-                        }) {
-                            SingleEntryView(viewModel: viewModel(forEntry: entry))
-                                .frame(width:nil, height:250)
-                        }.buttonStyle(PlainButtonStyle())
-                    case .video:
-                        Button(action: {
-                            coordinator.showVideo(atURL: entry.fileURL)
-                            videoActive = true
-                        }) {
-                            SingleEntryView(viewModel: viewModel(forEntry: entry))
-                                .frame(width:nil, height:250)
-                        }.buttonStyle(PlainButtonStyle())
-                    }
+                    view(forEntry: entry)
                 }
             }
         }
@@ -60,6 +37,7 @@ struct FilesView: View {
     }
     
     // MARK: - Private
+    
     private var coordinator: AppCoordinator
     private var emptyAction: () -> Void = {} // used on dir entries as you don't need an action
     
@@ -75,30 +53,34 @@ struct FilesView: View {
         coordinator.toogleFavorite(forFileEntry: entry)
     }
     
+    @ViewBuilder private func view(forEntry entry: FileEntry) -> some View {
+        switch entry.type {
+        case .directory:
+            Button(action:{
+                selectDirectory(entry:entry)}) {
+                    SingleEntryView(viewModel: viewModel(forEntry: entry))
+                }.buttonStyle(PlainButtonStyle())
+        case .image:
+            Button(action: {
+                coordinator.showImage(atURL: entry.fileURL)
+                imageActive = true
+            }) {
+                SingleEntryView(viewModel: viewModel(forEntry: entry))
+                    .frame(width:nil, height:250)
+            }.buttonStyle(PlainButtonStyle())
+        case .video:
+            Button(action: {
+                coordinator.showVideo(atURL: entry.fileURL)
+                videoActive = true
+            }) {
+                SingleEntryView(viewModel: viewModel(forEntry: entry))
+                    .frame(width:nil, height:250)
+            }.buttonStyle(PlainButtonStyle())
+        }
+    }
+    
     private func viewModel(forEntry entry: FileEntry) -> SingleEntryViewModel {
         SingleEntryViewModel(entry: entry, coordinator: coordinator)
-    }
-}
-
-// old implementation with List and NavigationLink
-extension FilesView {
-    private func listView() -> some View {
-        List(viewModel.entries) { entry in
-//            if entry.isDir {
-//                Button(action:{
-//                        selectDirectory(entry:entry)}) {
-//                    SingleEntryView(buttonAction:emptyAction, entry: entry)
-//                }.buttonStyle(PlainButtonStyle())
-//            }
-//            else {
-//                NavigationLink(
-//                    destination: SingleImageView(imageURL: entry.fileURL)) {
-//                    SingleEntryView(buttonAction:{
-//                        toggleFavorite(entry)
-//                    }, entry: entry)
-//                }
-//            }
-        }
     }
 }
 
